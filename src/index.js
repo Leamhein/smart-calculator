58712c07ea16e72754cd9f72e0000ed4c0f147b1
Vladimir Apenko <Leamhein@hotmail.com>
2018-02-27 22:18:46 +0300

    Done 285/301

--- src/index.js
+++ src/index.js
@@ -1,75 +1,70 @@
 class SmartCalculator {
   constructor(initialValue) {
-    this.array = [];
+    this.array = []; //make an array witch symbolized our equation
     this.array.push(initialValue);
   }
 
-  add(number) {
+  add(number) { //if "+" add it with number to our array
     this.array.push("+");
     this.array.push(number);
     return this;
-    console.log(this.array);
   }
 
   subtract(number) {
     this.array.push("-");
     this.array.push(number);
     return this;
-    console.log(this.array);
   }
 
   multiply(number) {
     this.array.push("*");
     this.array.push(number);
     return this;
-    console.log(this.array);
   }
 
   devide(number) {
     this.array.push("/");
     this.array.push(number);
     return this;
-    console.log(this.array);
   }
 
   pow(number) {
     this.array.push("^");
     this.array.push(number);
     return this;
-    console.log(this.array);
   }
-  calc (array) {
+  valueOf (array) {
     for (let i = 0; i < this.array.length; i++) {
-      if (this.array[i] == "^") {
-        this.array.splice(this.array[i-1], 3, Math.pow(this.array[i-1], this.array[i+1]));
+      if (this.array[i] == "^") { //find our pow
+        this.array.splice([i-1], 3, Math.pow(this.array[i-1], this.array[i+1])); //change the array elements to pow result
         i--;
       };
     };
     for (let i = 0; i < this.array.length; i++) {
-      if (this.array[i] == "/") {
-        this.array.splice(this.array[i-1], 3, this.array[i-1] / this.array[i+1]);
+      if (this.array[i] == "/") { // the same act with devide and so on...
+        this.array.splice([i-1], 3, this.array[i-1] / this.array[i+1]);
         i--;
       };
     };
     for (let i = 0; i < this.array.length; i++) {
       if (this.array[i] == "*") {
-        this.array.splice(this.array[i-1], 3, this.array[i-1] * this.array[i+1]);
+        this.array.splice([i-1], 3, this.array[i-1] * this.array[i+1]);
         i--;
       };
     };
     for (let i = 0; i < this.array.length; i++) {
       if (this.array[i] == "-") {
-        this.array.splice(this.array[i-1], 3, this.array[i-1] - this.array[i+1]);
+        this.array.splice([i-1], 3, this.array[i-1] - this.array[i+1]);
         i--;
       };
     };
     for (let i = 0; i < this.array.length; i++) {
       if (this.array[i] == "+") {
-        this.array.splice(this.array[i-1], 3, this.array[i-1] + this.array[i+1]);
+        this.array.splice([i-1], 3, this.array[i-1] + this.array[i+1]);
         i--;
       };
     };
-    return this.array[0];
+    return this.array[0]; // return first element of our array, which is  an answer
   };
 };
 
